57785fc9a19c486044e459f23bf80dcee506ce62
Anthony Pang <anthonypang99@gmail.com>
2022-05-03 10:36:49 -0400

    contact fix

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Contact = () => {
     return (
-        <div className='container contact' data-aos="fade-up">
+        <div id='#contact' className='container contact' data-aos="fade-up">
             <h1>Let's get in touch!</h1><p>Feel free to contact me through either my <a href='mailto:anthonypang99@gmail.com'><strong>Email</strong></a> or my <a href='https://www.linkedin.com/in/antpang/' target="_blank" rel="noreferrer"><strong>LinkedIn</strong></a></p></div>
     )
 }
