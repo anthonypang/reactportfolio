de6eeabc907adeed44d78a89bf392bec9ceaa9da
Anthony Pang <anthonypang99@gmail.com>
2022-06-10 20:35:13 -0400

    fix

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -2,9 +2,12 @@ import React from 'react'
 
 const About = () => {
     return (
-        <div id='about' className='container about shadow' data-aos="fade-up">
-            <h1>About Me</h1>
-            <p>Looking to make a difference in the world. I am always willing to learn new things! </p>
+        <div id='about' className='container about-container shadow' data-aos="fade-up">
+            <div className='about'>
+                <h1>About Me</h1>
+                <p>A recent graduate from the University of North Carolina at Charlotte. In my free time, I have worked on multiple side projects in order to leverage experience building responsive and scalable applications. I have experience in both front and back-end development using technologies such as JavaScript, React, NodeJS, and Express. </p>
+            </div>
+
         </div>
     )
 }
