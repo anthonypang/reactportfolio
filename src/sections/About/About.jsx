4d821d753657ff9e4e7b4fdcb321d7ccb1034586
Anthony Pang <anthonypang99@gmail.com>
2025-01-07 13:33:21 -0500

    update about section to include skills, leave room for image

--- src/sections/About/About.jsx
+++ src/sections/About/About.jsx
@@ -1,21 +1,44 @@
 import React from "react";
+import "../../styles/About.css";
 
 const About = () => {
   return (
-    <div
-      id="about"
-      className="container about-container shadow"
-      data-aos="fade-up"
-    >
+    <div id="about" className="container about-container" data-aos="fade-up">
       <div className="about">
-        <h1>About Me</h1>
-        <p>
-          A recent graduate from the University of North Carolina at Charlotte.
-          In my free time, I have worked on multiple side projects in order to
-          leverage experience building responsive and scalable applications. I
-          have experience in both front and back-end development using
-          technologies such as JavaScript, React, NodeJS, and Express.{" "}
-        </p>
+        <div>
+          <h1>About Me</h1>
+          <p>
+            Hi, I’m Anthony Pang, a Front-End Engineer with 2 years of
+            experience building sleek, user-focused web applications. I thrive
+            on creating performant, reusable components and optimizing
+            interfaces for both functionality and aesthetics.
+            <br />
+            <br />
+            What excites me most is solving challenging problems and turning
+            ideas into seamless digital experiences.
+            <br />
+            <br />
+            I’m passionate about staying ahead of industry trends and
+            collaborating with teams to deliver impactful solutions.
+          </p>
+          <br />
+          <hr />
+          <br />
+          <h3>Technologies</h3>
+          <div className="technologies">
+            <ul>
+              <li>TypeScript</li>
+              <li>ReactJS</li>
+              <li>NextJS</li>
+            </ul>
+            <ul>
+              <li>JavaScript</li>
+              <li>HTML</li>
+              <li>CSS</li>
+            </ul>
+          </div>
+        </div>
+        <div></div>
       </div>
     </div>
   );
