3fe81c602a5622c1ac81e2951e1261ed06a71f32
Anthony Pang <anthonypang99@gmail.com>
2025-02-19 14:20:08 -0500

    hamburger menu, disable autohide

--- src/sections/Header/Header.jsx
+++ src/sections/Header/Header.jsx
@@ -2,48 +2,53 @@ import React, { useState, useEffect } from "react";
 import "./Header.css";
 import { navItems } from "../../data/data";
 
-const Header = ({ toggleTheme, toggleMenu }) => {
+const Header = ({ toggleTheme }) => {
   const [scrolled, setScrolled] = useState(false);
-  const [visible, setVisible] = useState(true);
-  const [prevScrollPos, setPrevScrollPos] = useState(0);
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
   useEffect(() => {
     const handleScroll = () => {
-      const currentScrollPos = window.scrollY;
-
-      // Set scrolled state for background change
-      setScrolled(currentScrollPos > 50);
-
-      // Handle navbar show/hide
-      setVisible(
-        (prevScrollPos > currentScrollPos && currentScrollPos > 0) ||
-          currentScrollPos < 50
-      );
-
-      setPrevScrollPos(currentScrollPos);
+      setScrolled(window.scrollY > 50);
     };
 
     window.addEventListener("scroll", handleScroll);
     return () => window.removeEventListener("scroll", handleScroll);
-  }, [prevScrollPos]);
+  }, []);
+
+  const handleNavClick = (e, id) => {
+    e.preventDefault();
+    if (id === "") {
+      window.scrollTo({
+        top: 0,
+        behavior: "smooth",
+      });
+    } else {
+      document.getElementById(id)?.scrollIntoView({
+        behavior: "smooth",
+        block: "start",
+      });
+    }
+    setIsMenuOpen(false);
+  };
 
   return (
-    <header
-      className={`header ${scrolled ? "scrolled" : ""} ${
-        !visible ? "hidden" : ""
-      }`}
-    >
+    <header className={`header ${scrolled ? "scrolled" : ""}`}>
       <div className="nav-container">
-        {/* eslint-disable-next-line jsx-a11y/anchor-is-valid */}
-        <a href="#" className="logo">
+        <button onClick={(e) => handleNavClick(e, "")} className="logo">
           AP
-        </a>
-        <nav className="nav-links">
+        </button>
+        <nav className={`nav-links ${isMenuOpen ? "open" : ""}`}>
           {navItems.map((item, index) => (
-            <a key={index} href={`#${item.id}`}>
+            <button
+              key={index}
+              onClick={(e) => handleNavClick(e, item.id)}
+              className="nav-link"
+            >
               {item.title}
-            </a>
+            </button>
           ))}
+        </nav>
+        <div className="header-buttons">
           <button onClick={toggleTheme} className="theme-toggle">
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -53,7 +58,11 @@ const Header = ({ toggleTheme, toggleMenu }) => {
               <path d="M12 2.25a.75.75 0 01.75.75v2.25a.75.75 0 01-1.5 0V3a.75.75 0 01.75-.75zM7.5 12a4.5 4.5 0 119 0 4.5 4.5 0 01-9 0zM18.894 6.166a.75.75 0 00-1.06-1.06l-1.591 1.59a.75.75 0 101.06 1.061l1.591-1.59zM21.75 12a.75.75 0 01-.75.75h-2.25a.75.75 0 010-1.5H21a.75.75 0 01.75.75zM17.834 18.894a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 10-1.061 1.06l1.59 1.591zM12 18a.75.75 0 01.75.75V21a.75.75 0 01-1.5 0v-2.25A.75.75 0 0112 18zM7.758 17.303a.75.75 0 00-1.061-1.06l-1.591 1.59a.75.75 0 001.06 1.061l1.591-1.59zM6 12a.75.75 0 01-.75.75H3a.75.75 0 010-1.5h2.25A.75.75 0 016 12zM6.697 7.757a.75.75 0 001.06-1.06l-1.59-1.591a.75.75 0 00-1.061 1.06l1.59 1.591z" />
             </svg>
           </button>
-          <button className="menu-toggle" onClick={toggleMenu}>
+          <button
+            className="menu-toggle"
+            onClick={() => setIsMenuOpen(!isMenuOpen)}
+            aria-label="Toggle menu"
+          >
             <svg
               xmlns="http://www.w3.org/2000/svg"
               fill="none"
@@ -68,7 +77,7 @@ const Header = ({ toggleTheme, toggleMenu }) => {
               />
             </svg>
           </button>
-        </nav>
+        </div>
       </div>
     </header>
   );
